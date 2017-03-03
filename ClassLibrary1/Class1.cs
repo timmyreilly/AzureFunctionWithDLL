3472ab587e9a0fed90e098549d530fcf0c3500f6
Tim Reilly <timreilly@live.com>
2017-03-02 16:18:56 -0800

    working function with dll

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static void CallEbay()
+        {
+            var client = (eBayServiceReference1.ProductServicePortTypeClient)null;  
+            try
+            {
+                Console.Write("in call ebay"); 
+                client = new eBayServiceReference1.ProductServicePortTypeClient();
+                var productRequest = new eBayServiceReference1.AddProductsRequest();
+
+                client.addProducts(productRequest);
+            }
+            finally
+            {
+                client?.Close(); 
+            }
+        }
+    }
+}
---
+++ FunctionApp1/QueueTriggerCSharpEbay/run.csx
@@ -0,0 +1,14 @@
+﻿#r "..\bin\ClassLibrary1.dll"
+
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers; 
+using System.Threading.Tasks; 
+
+public static void Run(string myQueueItem, TraceWriter log)
+{
+    ClassLibrary1.Class1.CallEbay(); 
+
+    log.Info($"C# Queue trigger function processed: {myQueueItem}");
+}
+
